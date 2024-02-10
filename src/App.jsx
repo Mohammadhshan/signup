20d7c9dcdb9321374a21c1602d2229e3000abd12
NTPL-318 <shan.m@neoito.com>
2024-02-10 06:37:22 +0530

    password validation emoji updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
 import Signup from './pages/signup/Signup'
-
+import Logo from './assets/images/logo.png';
 function App() {
 
   return (
-   <Signup/>
+  <>
+    <header className="w-full float-left flex justify-between px-6 py-4">
+      <img src={Logo} alt=""/>
+    </header>
+    <div className="w-full float-start flex justify-center">
+        <div className="w-full float-start grid grid-cols-2 px-5 py-10 max-w-[1480px]">
+            <div>
+                &nbsp;
+            </div>
+            <div className=" w-full float-left">
+               <Signup/>   
+            </div>
+        </div>
+     </div>
+  </>
   )
 }
 
